c7d476b476b3fd03d93477e6e85bd7a25ab002b2
dembinski2019 <everton_s.d@hotmail.com>
2020-05-13 23:04:37 -0300

    modificando templates

--- core/tradicional.py
+++ core/tradicional.py
@@ -1,21 +1,66 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.dialog import MDDialog
 from kivy.uix.boxlayout import BoxLayout
-from kivymd.uix.button import MDFlatButton
+from kivymd.uix.button import MDFlatButton,MDRaisedButton
+from kivymd.uix.picker import MDTimePicker
+from kivymd.uix.menu import MDDropdownMenu
+
+class MateriasCadastradas(BoxLayout):
+    
+    def __init__(self,context={},**kwargs):
+        super().__init__(**kwargs)
+        self.ids.materia_cadastrada.text = context['materia']
+        self.ids.horario_cadastrado.text = context['horario']
+    
+    
 
 class Content(BoxLayout):
-    pass
+    lista_materias=['Português', 'Matematica', 'Fisica', 'Quimica', 'Biologia']
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        menu_items = [{ "text": f"{i}"} for i in self.lista_materias]
+        self.menu = MDDropdownMenu(
+            caller=self.ids.drop_item,
+            items=menu_items,
+            position="bottom",
+            callback=self.set_item,
+            width_mult=4,
+            
+        )
+
+    def set_item(self, instance):
+        self.materia = instance.text
+        self.ids.drop_item.set_item(instance.text)
+
+
+    def clock(self):
+        time_dialog = MDTimePicker()
+        time_dialog.bind(time=self.get_time)
+        time_dialog.open()
+
+    
+    def get_time(self, instance, time):
+        self.horario = str(time)
+        self.ids.time.text = str(time)
+
+    def add_materia(self):
+        context = {
+            'materia':self.materia,
+            'horario':self.horario
+        }
+        self.ids.box.add_widget(MateriasCadastradas(context))
 
 class Tradicional(Screen):
     def selecionarmaterias(self):
-        select = MDDialog(title = 'dialogo',
+        select = MDDialog(title = 'Cadastrar Materias',
         type="custom",
         content_cls=Content(),
-        buttons = [MDFlatButton(
+        buttons = [MDRaisedButton(text='OK',md_bg_color=(1, 0, 1, 1)),MDFlatButton(
                         text="CANCEL"
-                    )])
+                    )]
+        )
+        select.ids.container.height='500dp'
+        select.ver_growth = 'down'
         return select.open()
-
-
-
-
+    
+    
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager,SwapTransition
+from kivy.uix.screenmanager import ScreenManager
 from core.planodeestudo import PlanoDeEstudo
 from core.organizese import OrganizeSe
 from core.tradicional import Tradicional
-
 
 class Manager(ScreenManager):
     def transicao(self,*args):
         self.transition.direction = args[1]
         self.current=args[0]
-    
     
    
 
